116406361355427e5dd87e3e3c03f898e23b8528
MauricespazzoZeRo <mauricejnr161@gmail.com>
2023-08-11 22:37:12 +0000

    Args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv[1:]
+
+    argc = len(argv)
+
+    print(f"{argc} argument", end='')
+
+    if argc == 0:
+        print('.')
+    elif argc == 1:
+        print(':')
+    else:
+        print('s:')
+
+    for i, arg in enumerate(argv, start=1):
+        print(f"{i}: {arg}")
