abc6bd33567766be25c72501cea634ec30f32387
MauricespazzoZeRo <mauricejnr161@gmail.com>
2023-08-11 23:37:10 +0000

    Still here

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -3,13 +3,10 @@ def add(a, b):
     """My addition function
 
     Args:
-	a: first integer
-	b: second integer
+    a: first integer
+    b: second integer
 
     Returns:
-	The return value. a + b
+    The return value. a + b
     """
     return (a + b)
-
-if __name__ == "__main__":
-    main()
