06cbe86ef0665c7326753be2e5f11185d5b84651
MauricespazzoZeRo <mauricejnr161@gmail.com>
2023-08-12 15:20:36 +0000

    Let's see what's in you ;)

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def element_at(my_list, idx):
+    """
+    Retrieves an element from a list.
+
+    Args:
+        my_list (list): List with elements.
+        idx: Index for elements in list.
+    """
+    if idx < 0 or idx > len(my_list):
+        return None
+
+    return my_list[idx]
