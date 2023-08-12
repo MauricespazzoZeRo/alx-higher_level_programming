db793dfb096318fc2e264c4d38e5996f35167256
MauricespazzoZeRo <mauricejnr161@gmail.com>
2023-08-12 17:27:34 +0000

    Yeah

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -5,7 +5,5 @@ def print_list_integer(my_list=[]):
     """
     prints a list of given integers
     """
-    new_list = my_list.copy
-    new_list.reverse()
-    for i in len(new_list):
+    for i in my_list:
         print("{:d}".format(i))
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,7 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    for i in range(len(my_list) - 1, -1, -1):
-        print("{:d}".format(i))
+     new_list = my_list.copy
+     new_list.reverse()
+     for i in len(new_list):
+         print("{:d}".format(i))
