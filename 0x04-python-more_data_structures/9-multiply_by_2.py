c333d76c290b4a4fcc4e30fe34d3ecd6efc1cc5a
MauricespazzoZeRo <mauricejnr161@gmail.com>
2023-08-16 18:37:12 +0000

    Still doubling

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,9 +2,13 @@
 
 
 def multiply_by_2(a_dictionary):
+    return {key: value * 2 for (key, value) in a_dictionary.items()}
+"""
+---------------------OR-----------------------------
     new_dict = {}
 
     for key, val in a_dictionary.items():
         new_dict[key] = val * 2
 
     return new_dict
+"""
