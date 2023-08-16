388a5064d03bb6620fe9573ab700154a4c8c0470
MauricespazzoZeRo <mauricejnr161@gmail.com>
2023-08-16 05:58:50 +0000

    Simple square matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for idx in row:
+            new_row.append(idx ** 2)
+
+        new_matrix.apprend(new_row)
+
+    return (result_matrix)
