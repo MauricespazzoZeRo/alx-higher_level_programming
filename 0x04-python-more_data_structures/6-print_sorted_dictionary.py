8506af8e8b6cc96242c306046a488d0668f0dd93
MauricespazzoZeRo <mauricejnr161@gmail.com>
2023-08-16 17:39:28 +0000

    Well... Dictionaries are always arranged

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,5 +2,7 @@
 
 
 def print_sorted_dictionary(a_dictionary):
-    sorted_a = sorted(a_dictionary)
-    print(sorted_a)
+    sorted_a = sorted(a_dictionary.keys())
+
+    for key in sorted_a:
+        print(f"{key}: {a_dictionary[key]}")
