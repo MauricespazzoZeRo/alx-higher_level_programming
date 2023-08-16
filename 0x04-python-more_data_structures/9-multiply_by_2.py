755bcfe2a3d3d856a2047c540a3a6379d0570adc
MauricespazzoZeRo <mauricejnr161@gmail.com>
2023-08-16 18:40:24 +0000

    Doubling still

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,7 +2,13 @@
 
 
 def multiply_by_2(a_dictionary):
+    """
+    using dictionary comprehension for my first time
+    in order to multiply the value of every key: value
+    pair in a dictionary
+    """
     return {key: value * 2 for (key, value) in a_dictionary.items()}
+
 """
 ---------------------OR-----------------------------
     new_dict = {}
