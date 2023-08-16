4af51c7ad0c26d9c0e53ac061ed9831b35b31477
MauricespazzoZeRo <mauricejnr161@gmail.com>
2023-08-16 18:05:00 +0000

    We all know Molly

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,8 +2,9 @@
 
 
 def best_score(a_dictionary):
-    sorted_dict = dict(sorted(a_dictionary.item(), key=lambda item: item[1]))
-    sorted_vals = sorted_dict.values()
-    best_score = sorted_vals[-1]
+    if a_dictionary is None:
+        return None
 
-    return (best_score)
+    sorted_dict = dict(sorted(a_dictionary.items(), key=lambda item: item[1]))
+    best_key = list(sorted_dict.keys())[-1]
+    return best_key
