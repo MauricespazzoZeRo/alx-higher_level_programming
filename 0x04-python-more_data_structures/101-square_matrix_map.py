6b8092363d804553f44394df8fdcc43e63c18be8
MauricespazzoZeRo <mauricejnr161@gmail.com>
2023-08-16 18:26:25 +0000

    The matrix

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def suare_matrix_map(matrix=[]):
-    return list(map(lambda x: x ** 2, matrix))
+def square_matrix_map(matrix=[]):
+    return [list(map(lambda x: x ** 2, row)) for row in matrix]
