f4ff953c8f662e1e730498a5ae0cacf37c0f987a
MauricespazzoZeRo <mauricejnr161@gmail.com>
2023-08-17 11:36:38 +0000

    For Unique add

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -6,8 +6,8 @@ def uniq_add(my_list=[]):
     total = 0
 
     for element in my_list:
-            if element not in first_occ:
-                first_occ[element] = True
-                total += element
+        if element not in first_occ:
+            first_occ[element] = True
+            total += element
 
     return (total)
