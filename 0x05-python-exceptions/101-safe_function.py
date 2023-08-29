4f677115f9f693d56f24f0294df326d16328a0e7
MauricespazzoZeRo <mauricejnr161@gmail.com>
2023-08-29 12:30:21 +0000

    Rename directory

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except (TypeError, IndexError, ZeroDivisionError, ValueError) as e:
+        print("Exception:", e, file=sys.stderr)
+        return None
