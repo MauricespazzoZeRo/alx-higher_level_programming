9f65481ce63604f514996c7b3a884a32a89cc7da
MauricespazzoZeRo <mauricejnr161@gmail.com>
2023-09-05 20:50:20 +0000

    Module comment

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+My rectangle module
+"""
 
 class Rectangle():
     """empty rectangle class for later use
--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+My rectangle module
+"""
 
 class Rectangle():
     """rectangle class for storing rectangle data
