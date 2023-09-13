c1b56b0a7a04517e781f4e7345845119baa30e27
MauricespazzoZeRo <mauricejnr161@gmail.com>
2023-09-13 11:35:45 +0000

    Correction

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -13,8 +13,25 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """retrieves dict rep of Student in json form
         """
+        is_all_strs = True
+        if isinstance(attrs, list):
+            for ele in attrs:
+                if not isinstance(ele, str):
+                    is_all_strs = False
+        else:
+            is_all_strs = False
+
         return {key: value for (key, value) in self.__dict__.items()
-                if key in list(self.__dict__.keys())}
+                if key in list(self.__dict__.keys()) and
+                (not is_all_strs or key in attrs)}
+
+    def reload_from_json(self, json):
+        """reloads an object from a json dictionary
+            -> assumes json to always be a dictionary
+        """
+        self.first_name = json['first_name']
+        self.last_name = json['last_name']
+        self.age = json['age']
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+"""
+my module
+"""
+
 
 class Student:
     """student class for use
