1a093d91e5d81c99219041fe46926c8862f14fae
MauricespazzoZeRo <mauricejnr161@gmail.com>
2023-11-14 06:20:09 +0000

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+// Script that uses loops to print text
+
+let cMsg = 'C is fun';
+let PyMsg = 'Python is cool';
+let JvMsg = 'JavaScript is amazing';
+
+for (let i = 0; i < 1; i++)
+{
+	console.log(cMsg + '\n' + PyMsg + '\n' + JvMsg);
+}
