3950aac322ca4922a23a99cb7312261957f4883a
MauricespazzoZeRo <mauricejnr161@gmail.com>
2023-11-15 05:09:08 +0000

    9

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let count = 0; // Initialize a counter to keep track of the number of arguments printed
+
+exports.logMe = function (item) {
+  console.log(`${count}: ${item}`);
+  count++; // Increment the counter for each argument printed
+};
